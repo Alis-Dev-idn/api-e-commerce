c27d658ed4ef4cc1e002760db1a553b7c6abbb9a
Alis-Dev-idn <ali.dev313@gmail.com>
2023-01-10 15:04:39 +0700

    add route shop

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,16 @@
 import express, {Router} from "express";
 import UserController from "../controllers/user.controller.js";
 import Utils from "../utils/utils.js";
+import ShopController from "../controllers/shop.controller.js";
 
 class ExpressRoute {
     static route = express();
     static user = Router();
+    static shop = Router();
+
     static Route() {
         this.route.use("/user", this.User());
+        this.route.use("/shop", this.shopRoute());
 
         this.route.use('/favicon.ico', express.static('./public/assets/icon/favicon.ico'));
         this.route.use((req, res) => {
@@ -17,11 +21,20 @@ class ExpressRoute {
 
     static User() {
         this.user.get("/", UserController.GetUser);
-        this.user.post("/", Utils.middleware, UserController.CreateUser);
-        this.user.delete("/", UserController.deleteUser);
+        this.user.post("/", UserController.CreateUser);
+        this.user.delete("/", Utils.middleware, UserController.deleteUser);
         this.user.post("/login", UserController.userLogin);
 
         return this.user;
+    }
+
+    static shopRoute() {
+        this.shop.get("/", ShopController.getShop);
+        this.shop.post("/", Utils.middleware, ShopController.createShop);
+        this.shop.put("/", ShopController.updateShop);
+        this.shop.delete("/", ShopController.deleteShop);
+
+        return this.shop;
     }
 }
 
