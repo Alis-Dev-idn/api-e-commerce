f43c43c7770442c4c5ca4974afd41577e96bbca4
Alis-Dev-idn <ali.dev313@gmail.com>
2023-01-10 15:04:13 +0700

    add model shop data

--- src/models/models.js
+++ src/models/models.js
@@ -12,14 +12,32 @@ class Models {
             phone: Number,
             birthday: Number,
             active: Boolean,
-            shop: {
-                _id: false,
-                id: idShop,
-                name: String
-            }
+            shop: [
+                {
+                    _id: false,
+                    id: idShop,
+                    name: String
+                }
+            ]
         }, {timestamps: true});
 
         return mongoose.model("user", UserModel);
+    }
+
+    static shop() {
+        const shopModel = new mongoose.Schema({
+            name: String,
+            image: String,
+            description: String,
+            rating: Number,
+            operation: {
+                _id: false,
+                open: Date,
+                close: Date
+            }
+        }, {timestamps: true});
+
+        return mongoose.model("shop", shopModel);
     }
 }
 
